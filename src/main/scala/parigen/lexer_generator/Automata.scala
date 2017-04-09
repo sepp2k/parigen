7d3c5f7ef2e194f8ba1a1b8f3962e606014d792a
Sebastian Hungerecker <sepp2k@googlemail.com>
2017-04-09 23:17:22 +0200

    Keep working on equivalence classes for the lexer generator

--- src/main/scala/parigen/lexer_generator/Automata.scala
+++ src/main/scala/parigen/lexer_generator/Automata.scala
@@ -1,7 +1,7 @@
 package parigen.lexer_generator
 
 import parigen._
-import scala.collection.SortedSet
+import scala.collection.SortedMap
 import scala.annotation.tailrec
 
 object Automata {
@@ -19,17 +19,45 @@ object Automata {
         ???
     }
 
+    // A state doesn't have anything other than its identity. That is all we can do with a state is
+    // to check whether it's equal to another state.
+    // Thus we represent states using an emtpy class, which only has its object identity.
     class State
-    type Symbol = (Char, Char)
-    type Alphabet = SortedSet[Symbol]
+
+    // The symbols of our automata are numeric IDs of equivalence classes that represent all the
+    // characters that are equivalent to the automata. This way, if a regex contains a character
+    // range containing hundreds of characters, we don't need hundreds of transitions in our
+    // automaton.
+    type Symbol = Int
+
+    type EquivalenceClass = List[(Char, Char)]
+
+    type EquivalenceClassMap = Map[Symbol, EquivalenceClass]
+
+    type Alphabet = SortedMap[(Char, Char), Symbol]
+
+    // A lookup function to find a character's equivalence class could look like this:
+    // def lookup(alphabet: Alphabet, char: Char): Symbol = {
+    //     val ((start, end), id) = alphabet.until(char, char).max
+    //     if (start <= char && char <= end) id
+    //     else -1
+    // }
+    // However we don't need this here as we will only look up characters in the generated code.
 
     @tailrec
     def mergeAlphabets(alpha1: Alphabet, alpha2: Alphabet, acc: Alphabet): Alphabet = {
         if (alpha1.isEmpty) alpha2 ++ acc
         else if (alpha2.isEmpty) alpha1 ++ acc
         else {
-            // TODO
-            mergeAlphabets(???, ???, ???)
+            val ((start1, end1), id1) = alpha1.head
+            val ((start2, end2), id2) = alpha2.head
+            if (end1 < start2) {
+                mergeAlphabets(alpha1.tail, alpha2, acc + alpha1.head)
+            } else if (end2 < start1) {
+                mergeAlphabets(alpha1, alpha2.tail, acc + alpha2.head)
+            } else if (start1 < start2 && end1 < end2) {
+                ???
+            } else ???
         }
     }
 
