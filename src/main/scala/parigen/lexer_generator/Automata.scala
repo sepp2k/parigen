6cd7ceb06e36efadf432839256b907adbdbb9b17
Sebastian Hungerecker <sepp2k@googlemail.com>
2017-04-30 00:00:21 +0200

    Start work on regex to NFA translation

--- src/main/scala/parigen/lexer_generator/Automata.scala
+++ src/main/scala/parigen/lexer_generator/Automata.scala
@@ -1,18 +1,71 @@
 package parigen.lexer_generator
 
 import parigen._
-import scala.collection.SortedSet
+import scala.collection.{SortedSet, SortedMap}
 import scala.annotation.tailrec
 import TokenExtractor.TokenID
 
 object Automata {
-    case class Nfa(alphabet: Alphabet, states: Set[State], transitions: Map[(State, Symbol), Set[State]], startingStates: Set[State], acceptingStates: Set[State])
-    case class Dfa(alphabet: Alphabet, states: Set[State], transitions: Map[(State, Symbol), State], startingState: State, acceptingStates: Set[State])
+    type Symbol = Int
+    case class Nfa(
+        alphabet: IndexedAlphabet,
+        states: Set[State],
+        transitions: Map[(State, Symbol), Set[State]],
+        startingStates: Set[State],
+        acceptingStates: Map[State, TokenID]
+    ) {
+        def or(other: Nfa) = {
+            require(alphabet == other.alphabet)
+            Nfa(alphabet, states ++ other.states, transitions ++ other.transitions,
+                startingStates ++ other.startingStates, acceptingStates ++ other.acceptingStates)
+        }
+
+        def concat(other: Nfa) = {
+            require(alphabet == other.alphabet)
+            val trans = transitions.map {
+                case ((state, sym), targets) =>
+                    if (targets.exists(t => acceptingStates.isDefinedAt(t))) (state, sym) -> (targets ++ other.startingStates)
+                    else (state, sym) -> targets
+            }
+            Nfa(alphabet, states ++ other.states, trans, startingStates, other.acceptingStates)
+        }
+    }
+
+    case class Dfa(alphabet: IndexedAlphabet,
+        states: Set[State],
+        transitions: Map[(State, Symbol), State],
+        startingState: State,
+        acceptingStates: Map[State, TokenID])
 
     def regexesToNfa(regexes: List[(Ast.Expression, TokenID)]): Nfa = {
-        val alphabet = extractAlphabet(regexes.map(_._1))
-        // TODO
-        ???
+        val alphabet = indexAlphabet(extractAlphabet(regexes.map(_._1)))
+        val nfas = regexes.map { case (regex, tokenID) => regexToNfa(regex, tokenID, alphabet) }
+        nfas.foldLeft( Nfa(alphabet, Set(), Map(), Set(), Map()) )(_ or _)
+    }
+
+    def regexToNfa(regex: Ast.Expression, tokenID: TokenID, alphabet: IndexedAlphabet): Nfa = regex match {
+        case Ast.StringLit("") => {
+            val s = new State
+            Nfa(alphabet, Set(s), Map(), Set(s), Map(s -> tokenID))
+        }
+        case Ast.StringLit(str) => {
+            val s = new State
+            val a = new State
+            val sym = lookup(alphabet, str(0))
+            Nfa(alphabet, Set(s,a), Map((s, sym) -> Set(a)), Set(s), Map(a -> tokenID))
+        }
+        case Ast.CharacterClass(ranges, false) =>
+            ???
+        case Ast.CharacterClass(ranges, true) =>
+            ???
+        case Ast.KleeneStar(arg) =>
+            ???
+        case Ast.Or(lhs, rhs) =>
+            regexToNfa(lhs, tokenID, alphabet) or regexToNfa(rhs, tokenID, alphabet)
+        case Ast.Concattenation(lhs, rhs) =>
+            regexToNfa(lhs, tokenID, alphabet) concat regexToNfa(rhs, tokenID, alphabet)
+        case Ast.RuleName(_) =>
+            sys.error("Rule name in regex should have been rejected.")
     }
 
     def nfaToDfa(nfa: Nfa): Dfa = {
@@ -20,29 +73,44 @@ object Automata {
         ???
     }
 
-    // A state doesn't have anything other than its identity. That is all we can do with a state is
-    // to check whether it's equal to another state.
-    // Thus we represent states using an emtpy class, which only has its object identity.
+    /** A state doesn't have anything other than its identity. That is all we can do with a state is
+     *  to check whether it's equal to another state.
+     *
+     *  Thus we represent states using an emtpy class, which only has its object identity.
+     */
     class State
 
-    // An equivalence class is a pair of characters, representing an inclusive range of chatacters
-    // that are equivalent for the purposes of the automaton. That is if, for example, all characters
-    // between a and z would lead to the same transitions in all cases, ('a','z') would constitute an
-    // equivalence class.
-    // This way, if a regex contains a character range containing hundreds of characters, we don't
-    // need hundreds of transitions in our automaton.
+    /** An equivalence class is a pair of characters, representing an inclusive range of chatacters
+     *  that are equivalent for the purposes of the automaton. That is if, for example, all characters
+     *  between a and z would lead to the same transitions in all cases, ('a','z') would constitute an
+     *  equivalence class.
+     *
+     *  This way, if a regex contains a character range containing hundreds of characters, we don't
+     *  need hundreds of transitions in our automaton.
+     */
     type EquivalenceClass = (Char, Char)
 
     type Alphabet = SortedSet[EquivalenceClass]
+    type IndexedAlphabet = SortedMap[EquivalenceClass, Symbol]
     val emptyAlphabet: Alphabet = SortedSet[EquivalenceClass]()
 
-    // A lookup function to find a character's equivalence class could look like this:
-    // def lookup(alphabet: Alphabet, char: Char): Symbol = {
-    //     val ((start, end), id) = alphabet.until(char, char).max
-    //     if (start <= char && char <= end) id
-    //     else -1
-    // }
-    // However we don't need this here as we will only look up characters in the generated code.
+    /** Translate a character to a symbol, which is its index in the list of equivalence classes. */
+    def lookup(alphabet: IndexedAlphabet, char: Char): Symbol = {
+        val ((start, end), id) = alphabet.until(char, char).max
+        if (start <= char && char <= end) id
+        else sys.error(s"Character $char not found in alphabet.")
+    }
+
+    /** Translate a range of characters to one or more symbols, which are the corresponding indices
+     *  in the list of equivalence classes.
+     */
+    def lookup(alphabet: IndexedAlphabet, range: (Char, Char)): Seq[Symbol] = {
+        val (from, to) = range
+        val ((fromStart, fromEnd), fromId) = alphabet.until(from, from).max
+        val ((toStart, toEnd), toId) = alphabet.until(to, to).max
+        if (fromStart <= from && from <= fromEnd && toStart <= to && to <= toEnd) fromId.to(toId)
+        else sys.error(s"Characters $from or $to not found in alphabet.")
+    }
 
     private def pred(c: Char): Char = (c - 1).toChar
     private def succ(c: Char): Char = (c + 1).toChar
@@ -112,6 +180,10 @@ object Automata {
         regexes.map(extractAlphabet).foldLeft(emptyAlphabet) {
             (acc, alpha) => mergeAlphabets(acc, alpha, emptyAlphabet)
         }
+    }
+
+    def indexAlphabet(alphabet: Alphabet): IndexedAlphabet = {
+        SortedMap(alphabet.zipWithIndex.toSeq : _*)
     }
 
     def regexesToDfa(regexes: List[(Ast.Expression, TokenID)]): Dfa = {
