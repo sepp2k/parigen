25c35de03ec316426cf70bae1966a2b0599335c5
Sebastian Hungerecker <sepp2k@googlemail.com>
2017-05-03 20:01:20 +0200

    Fix index error in main method

--- src/main/scala/parigen/Test.scala
+++ src/main/scala/parigen/Test.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 object Test {
     def main(args: Array[String]): Unit = {
         val filename =
-            if (args.length > 0) args(1)
+            if (args.length > 0) args(0)
             else "src/main/parigen/parigen.pig"
         val source = Source.fromFile(filename)
         val g = try source.mkString finally source.close()
