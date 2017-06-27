66c176cc335200ba01ba72498e118387c973ed20
Sebastian Hungerecker <sepp2k@googlemail.com>
2017-06-28 00:31:24 +0200

    Use SexyOpt for command line parsing

--- build.sbt
+++ build.sbt
@@ -4,4 +4,7 @@ version := "1.0"
 
 scalaVersion := "2.12.1"
 
+resolvers += Resolver.sonatypeRepo("snapshots")
+
 libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.5"
+libraryDependencies += "com.github.sepp2k" %% "sexyopt" % "0.1-SNAPSHOT"
---
+++ src/main/scala/parigen/Main.scala
@@ -0,0 +1,23 @@
+package parigen
+
+import scala.io.Source
+import sexyopt.SexyOpt
+
+object Test extends SexyOpt {
+    override val programName = "parigen"
+    override val programDescription = "Generate a lexer, a parser and IDE support from a grammar."
+
+    val displayGraphs = flag("display-graphs", "Display the graphs of the generated automata directly using `dot -Tx11` instead of generating dot files.")
+    val filename = optionalPosArg("filename", "The name of the file containing the parigen grammar.", "src/main/parigen/parigen.pig")
+
+    def main(args: Array[String]): Unit = {
+        parse(args)
+        val debugMode = if (displayGraphs.value) Parigen.DebugMode.DisplayGraphs else Parigen.DebugMode.GraphsToFiles
+        val source = Source.fromFile(filename.value)
+        val g = try source.mkString finally source.close()
+        println("Input:")
+        println(g)
+        val diags = Parigen.compile(g, debugMode)
+        diags.foreach(System.err.println)
+    }
+}
