10588b1944bcf3fb5119a6e0c83f31ce23028618
Sebastian Hungerecker <sepp2k@googlemail.com>
2017-07-15 03:37:15 +0200

    Add new options and write generated code to file

--- src/main/scala/parigen/Main.scala
+++ src/main/scala/parigen/Main.scala
@@ -3,21 +3,25 @@ package parigen
 import scala.io.Source
 import sexyopt.SexyOpt
 
-object Test extends SexyOpt {
+object Main extends SexyOpt {
     override val programName = "parigen"
     override val programDescription = "Generate a lexer, a parser and IDE support from a grammar."
 
-    val displayGraphs = flag("display-graphs", "Display the graphs of the generated automata directly using `dot -Tx11` instead of generating dot files.")
+    val outDir = option("out-dir", 'o', "The directory in which the generated source files will be placed (default: the current directory)", ".")
+    val writeGraphs = flag("write-graphs", "Write the graphs of the generated automata to temporary dot files for debugging purposes.")
+    val displayGraphs = flag("display-graphs", "Display the graphs of the generated automata directly using `dot -Tx11` (requires GraphViz and X11).")
+    val printAst = flag("print-ast", "Print the AST of the input grammar to the screen for debugging purposes")
+    val printAlphabet = flag("print-alphabet", "Print the generated alphabet mapping to the screen for debugging purposes")
+    val printTokens = flag("print-tokens", "Print the Tokens' names and IDs to the screen for debugging purposes")
     val filename = optionalPosArg("filename", "The name of the file containing the parigen grammar.", "src/main/parigen/parigen.pig")
 
     def main(args: Array[String]): Unit = {
         parse(args)
-        val debugMode = if (displayGraphs) Parigen.DebugMode.DisplayGraphs else Parigen.DebugMode.GraphsToFiles
         val source = Source.fromFile(filename)
         val g = try source.mkString finally source.close()
-        println("Input:")
-        println(g)
-        val diags = Parigen.compile(g, debugMode)
+        val debug = Parigen.DebugOptions(writeGraphs = writeGraphs, displayGraphs = displayGraphs, printAst = printAst,
+                                         printAlphabet = printAlphabet, printTokens = printTokens)
+        val diags = Parigen.compile(g, outDir, debug)
         diags.foreach(System.err.println)
     }
 }
--- src/main/scala/parigen/Parigen.scala
+++ src/main/scala/parigen/Parigen.scala
@@ -2,21 +2,16 @@ package parigen
 
 import java.nio.file.Files
 import java.nio.charset.StandardCharsets.UTF_8
+import java.io.PrintStream
 import lexer_generator.LexerGenerator
 
 object Parigen {
-    sealed abstract class DebugMode
-    object DebugMode {
-        case object None extends DebugMode
-        case object GraphsToFiles extends DebugMode
-        case object DisplayGraphs extends DebugMode
-    }
+    case class DebugOptions(writeGraphs: Boolean, displayGraphs: Boolean, printAst: Boolean, printAlphabet: Boolean, printTokens: Boolean)
 
-    def compile(src: String, debugMode: DebugMode = DebugMode.None) = {
-        val printStages = debugMode != DebugMode.None
+    def compile(src: String, outDir: String, debug: DebugOptions) = {
         Parser.parse(src) match {
             case Parser.Success(ast, _) =>
-                if (printStages) {
+                if (debug.printAst) {
                     System.err.println("AST:")
                     System.err.println(ast)
                 }
@@ -24,28 +19,32 @@ object Parigen {
                 if (diags.exists(_.severity == Validator.Error)) diags
                 else {
                     val lexer = LexerGenerator.generateLexer(ast)
-                    if(printStages) {
+                    if (debug.printTokens) {
                         println("Token IDs:")
                         lexer.tokens.foreach(println)
+                    }
+                    if (debug.printAlphabet) {
                         println("Alphabet:")
                         lexer.alphabet.foreach {
                             case ((from, to), id) => println(s"$from .. $to -> $id")
                         }
-
-                        new plang.TypeScriptGenerator(System.out, 4).generate(lexer.code)
-
-                        if (debugMode == DebugMode.GraphsToFiles) {
-                            val nfaFile = Files.createTempFile("nfa", ".dot")
-                            Files.write(nfaFile, util.AutomataVisuzualizer.automatonToDot(lexer.nfa).getBytes(UTF_8))
-                            println(s"Nfa written to $nfaFile")
-                            val dfaFile = Files.createTempFile("dfa", ".dot")
-                            Files.write(dfaFile, util.AutomataVisuzualizer.automatonToDot(lexer.dfa).getBytes(UTF_8))
-                            println(s"Dfa written to $dfaFile")
-                        } else {
-                            util.AutomataVisuzualizer.displayAutomaton(lexer.nfa)
-                            util.AutomataVisuzualizer.displayAutomaton(lexer.dfa)
-                        }
                     }
+                    if (debug.writeGraphs) {
+                        val nfaFile = Files.createTempFile("nfa", ".dot")
+                        Files.write(nfaFile, util.AutomataVisuzualizer.automatonToDot(lexer.nfa).getBytes(UTF_8))
+                        println(s"Nfa written to $nfaFile")
+                        val dfaFile = Files.createTempFile("dfa", ".dot")
+                        Files.write(dfaFile, util.AutomataVisuzualizer.automatonToDot(lexer.dfa).getBytes(UTF_8))
+                        println(s"Dfa written to $dfaFile")
+                    }
+                    if (debug.displayGraphs) {
+                        util.AutomataVisuzualizer.displayAutomaton(lexer.nfa)
+                        util.AutomataVisuzualizer.displayAutomaton(lexer.dfa)
+                    }
+                    val outFile = new PrintStream(s"$outDir/lexer.ts")
+                    new plang.TypeScriptGenerator(outFile, 4).generate(lexer.code)
+                    outFile.close
+                    println(s"Lexer written to $outDir/lexer.ts")
                     diags
                 }
             case Parser.NoSuccess(message, rest) =>
