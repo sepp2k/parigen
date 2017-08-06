f470457a2a96de7dcbb5f214cec0d0b6229d92de
Sebastian Hungerecker <sepp2k@googlemail.com>
2017-08-06 18:50:08 +0200

    Prevent recompuation of already computed first sets

--- src/main/scala/parigen/parser_generator/ParserGenerator.scala
+++ src/main/scala/parigen/parser_generator/ParserGenerator.scala
@@ -21,28 +21,30 @@ import ParserGenerator._
 
 class ParserGenerator(grammar: SimplifiedGrammar.Grammar) {
     def firstSets: FirstSets = {
-        grammar.nonTerminals.map { case (name, body) =>
-            name -> firstSet(body)
+        grammar.nonTerminals.foldLeft(Map[String, FirstSet]()) { case (acc, (name, body)) =>
+            acc + (name -> firstSet(body, acc))
         }
     }
 
-    def firstSet(exp: SimplifiedGrammar.Expression): FirstSet = {
+    def firstSet(exp: SimplifiedGrammar.Expression, alreadyComputed: FirstSets): FirstSet = {
         exp.alternatives.flatMap {
             case SimplifiedGrammar.Concatenation(Seq()) => Seq(None)
-            case concatenation => firstSetForConcat(concatenation.items)
+            case concatenation => firstSetForConcat(concatenation.items, alreadyComputed)
         }.toSet
     }
 
-    def firstSetForConcat(items: Seq[SimplifiedGrammar.Item]): FirstSet = items match {
+    def firstSetForConcat(items: Seq[SimplifiedGrammar.Item], alreadyComputed: FirstSets): FirstSet = items match {
         case Seq() => Set(None)
         case Seq(SimplifiedGrammar.Terminal(name), _ @ _*) =>
             Set(Some(grammar.terminals(name).id))
         case Seq(SimplifiedGrammar.NonTerminal(name), tail @ _*) =>
-            val set = firstSet(grammar.nonTerminals(name))
-            if (set.contains(None)) {
-                (set - None) ++ firstSetForConcat(tail)
-            } else {
-                set
-            }
+            alreadyComputed.getOrElse(name, {
+                val set = firstSet(grammar.nonTerminals(name), alreadyComputed)
+                if (set.contains(None)) {
+                    (set - None) ++ firstSetForConcat(tail, alreadyComputed)
+                } else {
+                    set
+                }
+            })
     }
 }
